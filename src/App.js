ec5240aeea2f4f2da06cf4c42f2721c248293c6f
ranjit1999 <201751041@iiitvadodara.ac.in>
2021-01-11 21:55:35 +0530

    My App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+import BottomAppBar from './BottomAppBar.js';
+
+function App() {
+  return (
+    <div className="App">
+          
+            <BottomAppBar/>
+           
+    </div>
+  );
+}
+
+export default App;
